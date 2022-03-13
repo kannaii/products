7a29d7debacbe6fb0d60e40276c1670d15dabc78
Eden Chang <cornma1212@gmail.com>
2022-03-13 15:03:32 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,13 @@
+products = []
+
+while True:
+	name = input('請輸入商品名稱：')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格：')
+	# p = [] # 要同時把商品跟價格放進一個清單中, 先創一個清單p
+	# p.append(name)
+	# p.append(price)
+	# p = [name, price] # 上面3行快寫成這一行
+	products.append([name, price]) #products.append(p) 再把小清單p放進大清單products # 上面4行快寫成這一行
+print(products)
